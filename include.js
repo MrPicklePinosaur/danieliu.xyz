bb678de7cfdbc9a449e3ebd3f92c9977d1750713
Daniel Liu <mr.picklepinosaur@gmail.com>
2020-12-25 21:58:43 -0500

    clean up css + resources

---
+++ include.js
@@ -0,0 +1,11 @@
+document.getElementById("header-banner").innerHTML = `
+    <div id="left-nav">
+    <div class="nav-link"><a href="https://danieliu.xyz">home</a></div>
+    <div class="nav-link"><a href="https://danieliu.xyz/resources.html">resources</a></div>
+    </div>
+    <div id="right-nav">
+    <div class="nav-link"><a href="https://blog.danieliu.xyz">blog</a></div>
+    <div class="nav-link"><a href="https://git.danieliu.xyz">git</a></div>
+    <div class="nav-link"><a href="https://searx.danieliu.xyz">searx</a></div>
+    </div>
+`
