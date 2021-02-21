7cbc08ac0542ff6855e79deb577c6d668b1acfbd
Daniel Liu <mr.picklepinosaur@gmail.com>
2021-02-21 12:09:11 -0500

    update reosurce

--- include.js
+++ include.js
@@ -1,9 +1,11 @@
 document.getElementById("header-banner").innerHTML = `
     <div id="left-nav">
     <div class="nav-link"><a href="https://danieliu.xyz">home</a></div>
+    <!--div class="nav-link"><a href="https://danieliu.xyz/resume">resume</a></div-->
     <div class="nav-link"><a href="https://danieliu.xyz/resources.html">resources</a></div>
     </div>
     <div id="right-nav">
+    <!--div class="nav-link"><a href="https://repo.danieliu.xyz">repo</a></div-->
     <div class="nav-link"><a href="https://blog.danieliu.xyz">blog</a></div>
     <div class="nav-link"><a href="https://git.danieliu.xyz">git</a></div>
     <div class="nav-link"><a href="https://searx.danieliu.xyz">searx</a></div>
